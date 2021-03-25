239d6f221ddcfb60fd0594ad484ab1010930940d
Dennis Bragagnolo <ddns1002@gmail.com>
2021-03-25 19:54:52 -0300

    Correção do lab 4(tarefas)

--- tarefas/src/tarefas.c
+++ tarefas/src/tarefas.c
@@ -2,14 +2,14 @@
 #include "driverleds.h" // device drivers
 #include "cmsis_os2.h" // CMSIS-RTOS
 
-osThreadId_t thread1_id, thread2_id;
+osThreadId_t thread1_id, thread2_id, thread3_id, thread4_id;
 
 typedef struct  {
-  uint32_t led; //led que será utilizado na task
-  uint32_t delay; //período de delay da task
+  uint32_t led;               // led que será utilizado na task
+  uint32_t delay;             // período de delay da task
 } thread_arg;
 
-void thread(void *arg){ //neste momento, arg = endereço de onde está a struct
+void thread(void *arg){       // Passagem dos parametros (struct) por ponteiro
   uint8_t state = 0;
   
   thread_arg *aux;
@@ -17,24 +17,11 @@ void thread(void *arg){ //neste momento, arg = endereço de onde está a struct
   
   uint8_t led = (uint8_t) aux->led;
   uint32_t delay = aux->delay;
-  
-  switch (led){
-    case 1: led = LED1;
-            break;
-    case 2: led = LED2;
-            break;
-    case 3: led = LED3;
-            break;
-    case 4: led = LED4;
-            break;
-  };
-  
-  while(1){
-    
+
+  while(1){                   // Loop de execução da thread    
     state ^= led;
     LEDWrite(led, state);
-    osDelay(delay);
-    
+    osDelay(delay);    
   } // while
 } // thread
 
@@ -51,8 +38,8 @@ void main(void){
   
   thread1_id = osThreadNew(thread, (void *) &arg1, NULL);
   thread2_id = osThreadNew(thread, (void *) &arg2, NULL);
-  thread1_id = osThreadNew(thread, (void *) &arg3, NULL);
-  thread2_id = osThreadNew(thread, (void *) &arg4, NULL);
+  thread3_id = osThreadNew(thread, (void *) &arg3, NULL);
+  thread4_id = osThreadNew(thread, (void *) &arg4, NULL);
   
   if(osKernelGetState() == osKernelReady)
     osKernelStart();
